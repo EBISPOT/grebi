82f651c476770fe0de33a555dd00a116cbd40136
James McLaughlin <james@mclgh.net>
2024-12-18 17:12:35 +0000

    hacky test

--- dataload/07_create_db/sqlite/grebi_make_sqlite/src/main.rs
+++ dataload/07_create_db/sqlite/grebi_make_sqlite/src/main.rs
@@ -4,6 +4,7 @@ use rusqlite::Statement;
 use rusqlite::ToSql;
 use std::io::BufReader;
 use std::io::BufRead;
+use std::io::BufWriter;
 use std::io::StdinLock;
 use std::io;
 use std::io::Write;
@@ -42,7 +43,12 @@ fn insert(
 
 
     let mut n:i64 = 0;
-    let mut param_stack: Vec<_> = Vec::new();
+
+
+    let mut start_time3 = std::time::Instant::now();
+
+    let mut buf:Vec<u8> = Vec::new();
+    let mut param_locs: Vec<(usize, usize)> = Vec::new();
 
     loop {
 
@@ -50,44 +56,52 @@ fn insert(
         reader.read_until(b'\n', &mut line).unwrap();
 
         if line.len() == 0 {
-            eprintln!("saw {} subjects", n);
+            eprintln!("saw {} lines", n);
             break;
         }
 
-        let id = {
-            let tmp_id = get_id(&line);
-            tmp_id.to_owned()
-        };
+        let id_start = buf.len();
+        let id = get_id(&line);
+        buf.extend(id.iter());
+        let id_end = buf.len();
 
-        let compressed =  {
-            let mut enc = lz4::EncoderBuilder::new().level(compression_level).build(Vec::new()).unwrap();
+        let data_start = buf.len();
+        {
+            let mut enc = lz4::EncoderBuilder::new().level(compression_level).build(BufWriter::new(&mut buf)).unwrap();
             enc.write(&line).unwrap();
-            let (v, r) = enc.finish();
-            r.unwrap();
-            v
-        };
+            enc.finish().1.unwrap();
+        }
+        let data_end = buf.len();
 
-        param_stack.push(id);
-        param_stack.push(compressed);
+        param_locs.push((id_start, id_end));
+        param_locs.push((data_start, data_end));
 
-        if param_stack.len() == 2*batch_size {
-            stmt_batch.execute(rusqlite::params_from_iter(param_stack.iter())).unwrap();
-            param_stack.clear();
+        if param_locs.len() == 2*batch_size {
+            stmt_batch.execute(
+                rusqlite::params_from_iter(
+                    param_locs.iter().map(|(start, end)| &buf[*start..*end])
+                )
+            ).unwrap();
+            param_locs.clear();
+            buf.clear();
         }
 
         n = n + 1;
 
         if n % 1000000 == 0 {
-            eprintln!("{}", n);
+            eprintln!("inserted {}                  [last 1m took {} seconds]", n, start_time3.elapsed().as_secs());
+            start_time3 = std::time::Instant::now();
         }
     }
 
     // insert the last ones if we didn't reach batch size
-    if param_stack.len() > 0 {
+    if param_locs.len() > 0 {
         let mut i = 0;
-        while i < param_stack.len() {
-            let id = &param_stack[i];
-            let val = &param_stack[i+1];
+        while i < param_locs.len() {
+            let id_loc = param_locs[i];
+            let val_loc = param_locs[i+1];
+            let id = &buf[id_loc.0..id_loc.1];
+            let val = &buf[val_loc.0..val_loc.1];
             stmt_single.execute(params![id, val]).unwrap();
             i += 2;
         }
